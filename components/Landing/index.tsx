d019ce06acd07969de1fa3a980c609a20c1e3862
Enrique Gimenez <enrique.gimenez@edge.com.py>
2021-11-26 11:49:51 -0300

    fixed landing page - added logo

--- components/Landing/index.tsx
+++ components/Landing/index.tsx
@@ -1,12 +1,14 @@
-import LogButton from "../Buttons/LogButton"
-import Image from "next/image"
-import { backgroundImg, bgImgUrl } from "../../helpers/variables"
-import { LandingProps } from "../../types/types"
-import { styles } from "../../styles/styles"
+import LogButton from '../Buttons/LogButton'
+import { backgroundImg } from '../../helpers/variables'
+import Logo from '../../public/logo-curvas.svg'
+import Image from 'next/dist/client/image'
+import { LandingProps } from '../../types/types'
+import { styles } from '../../styles/styles'
 
 export default function Landing({ auth }: LandingProps) {
   return (
     <div className={styles.index.loggedOut} style={backgroundImg}>
+      <Image src={Logo} alt="Company's logo" />
       <LogButton auth={auth} />
     </div>
   )
--- styles/styles.ts
+++ styles/styles.ts
@@ -1,22 +1,23 @@
 export const styles = {
   nav: {
-    navbar: "navbar navbar-expand-lg navbar-light bg-light",
+    navbar: 'navbar navbar-expand-lg navbar-light bg-light',
   },
-  footer: "text-center bg-light text-secondary mt-3 py-3",
+  footer: 'text-center bg-light text-secondary mt-3 py-3',
   buttons: {
-    button: "btn btn-outline-dark",
-    background: "d-flex my-3 mx-3 justify-content-center",
-    cargarButton: "d-grid gap-2 my-3 col-6 mx-auto",
+    button: 'btn btn-outline-dark',
+    background: 'd-flex my-3 mx-3 justify-content-center',
+    cargarButton: 'd-grid gap-2 my-3 col-6 mx-auto',
   },
   spinner: {
-    div: "container-fluid d-flex align-items-center justify-content-center vh-100 w-25",
-    border: "spinner-border text-secondary",
-    span: "visually-hidden",
+    div: 'container-fluid d-flex align-items-center justify-content-center vh-100 w-25',
+    border: 'spinner-border text-secondary',
+    span: 'visually-hidden',
   },
   index: {
-    loggedOut: "container-fluid d-flex align-items-center w-sm-25 vh-100",
+    loggedOut:
+      'container-fluid d-flex flex-column align-content-center w-sm-25 vh-100',
   },
   tables: {
-    generic: "container-fluid w-75 my-3 text-center",
+    generic: 'container-fluid w-75 my-3 text-center',
   },
 }
